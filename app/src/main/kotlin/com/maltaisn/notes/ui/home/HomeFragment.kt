maltaisn/another-notes-app

commit 8452c731bfc2df481eb5e8b047ba055247f50dcc

Add reminder dialog
- Add "add/edit reminder" item to note and edit fragments.
- Replace all `DateTimeConverter.toDate` with dedicated test extension `dateFor`.
- Add special test `assertLiveDataEventSent` overload for Event<Unit>.
- Fix NavController.navigateSafe for global actions and nested graphs.
- Fix RecurrenceConverter not handling null values.

app/src/main/res/menu/menu_drawer.xml
@@ -18,7 +18,7 @@
     xmlns:android="http://schemas.android.com/apk/res/android">
 
     <group
-        android:id="@+id/group_note_location"
+        android:id="@+id/group_main_notes"
         android:checkableBehavior="single"
         >
 
@@ -28,6 +28,17 @@
             android:icon="@drawable/ic_text"
             android:title="@string/note_location_active"
             />
+
+        <item
+            android:id="@+id/item_reminders"
+            android:icon="@drawable/ic_alarm"
+            android:title="@string/note_reminders"
+            />
+
+    </group>
+
+    <group
+        android:id="@+id/group_other_notes">
 
         <item
             android:id="@+id/item_location_archived"

app/build.gradle
@@ -104,7 +104,8 @@ android {
 
 dependencies {
     // App dependencies
-    implementation "androidx.core:core-ktx:$androidKtxVersion"
+    implementation "androidx.core:core-ktx:$coreKtxVersion"
+    implementation "androidx.fragment:fragment-ktx:$fragmentKtxVersion"
     implementation "androidx.appcompat:appcompat:$appCompatVersion"
     implementation "androidx.recyclerview:recyclerview:$recyclerViewVersion"
     implementation "androidx.constraintlayout:constraintlayout:$constraintLayoutVersion"

app/src/androidTest/kotlin/com/maltaisn/notes/model/NotesDaoTest.kt
@@ -20,7 +20,7 @@ import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.maltaisn.notes.model.converter.DateTimeConverter
+import com.maltaisn.notes.dateFor
 import com.maltaisn.notes.model.entity.Note
 import com.maltaisn.notes.model.entity.NoteStatus
 import com.maltaisn.notes.model.entity.PinnedStatus
@@ -90,7 +90,7 @@ class NotesDaoTest {
     fun getByStatusTest() = runBlocking {
         // Add 5 notes for each status, ordered by descending last modified time. Also add a pinned active note first.
         // Then compare these notes with the result of the database query.
-        val baseDate = DateTimeConverter.toDate("2020-01-01T00:00:00.000Z")
+        val baseDate = dateFor("2020-01-01T00:00:00.000Z")
         val activeNotes = mutableListOf<Note>()
 
         val pinnedNote = testNote(id = 1, status = NoteStatus.ACTIVE, added = baseDate,
@@ -129,14 +129,13 @@ class NotesDaoTest {
             "2020-01-01T00:00:00.000Z",
             "2020-01-02T00:00:00.000Z")
         val notes = dates.mapIndexed { i, dateStr ->
-            val date = DateTimeConverter.toDate(dateStr)
+            val date = dateFor(dateStr)
             testNote(id = i + 1L, added = date, modified = date,
                 status = NoteStatus.DELETED)
         }
         notesDao.insertAll(notes)
 
-        val queryNotes = notesDao.getByStatusAndDate(NoteStatus.DELETED,
-            DateTimeConverter.toDate("2020-01-01T00:00:00.000Z"))
+        val queryNotes = notesDao.getByStatusAndDate(NoteStatus.DELETED, dateFor("2020-01-01T00:00:00.000Z"))
         assertEquals(notes.subList(0, 4).toSet(), queryNotes.toSet())
     }
 

app/src/main/kotlin/com/maltaisn/notes/FragmentExtensions.kt
@@ -1,0 +1,48 @@
+/*
+ * Copyright 2020 Nicolas Maltais
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.maltaisn.notes
+
+import android.app.Dialog
+import android.graphics.Rect
+import android.view.View
+import android.widget.FrameLayout
+import androidx.fragment.app.FragmentManager
+
+/**
+ * Returns whether this fragment manager contains a fragment with a [tag].
+ */
+operator fun FragmentManager.contains(tag: String) = this.findFragmentByTag(tag) != null
+
+/**
+ * Set [this] dialog maximum width to [maxWidth].
+ * @param view The dialog's content view.
+ */
+fun Dialog.setMaxWidth(maxWidth: Int, view: View) {
+    // Get current dialog's width and padding
+    val fgPadding = Rect()
+    val window = this.window!!
+    window.decorView.background.getPadding(fgPadding)
+    val padding = fgPadding.left + fgPadding.right
+    var width = this.context.resources.displayMetrics.widthPixels - padding
+
+    // Set dialog's dimensions, with maximum width.
+    if (width > maxWidth) {
+        width = maxWidth
+    }
+    window.setLayout(width + padding, FrameLayout.LayoutParams.WRAP_CONTENT)
+    view.layoutParams = FrameLayout.LayoutParams(width, FrameLayout.LayoutParams.WRAP_CONTENT)
+}

app/src/main/kotlin/com/maltaisn/notes/NavigationExtensions.kt
@@ -18,12 +18,23 @@ package com.maltaisn.notes
 
 import androidx.navigation.NavController
 import androidx.navigation.NavDirections
+import androidx.navigation.NavGraph
 
+/**
+ * Navigate safely to another destination with [directions]. If the action is marked as single top,
+ * this will prevent adding the same destination to the backstack more than once.
+ */
 fun NavController.navigateSafe(directions: NavDirections) {
-    try {
-        this.navigate(directions)
-    } catch (e: IllegalArgumentException) {
-        // "navigation destination ___ is unknown to this NavController".
-        // This happens if user presses two buttons at the same time for example.
+    // Get action by ID. If action doesn't exist, return.
+    val action = (currentDestination ?: graph).getAction(directions.actionId) ?: return
+    var destId = action.destinationId
+    val dest = graph.findNode(destId)
+    if (dest is NavGraph) {
+        // Action destination is a nested graph, which isn't a real destination.
+        // The real destination is the start destination of that graph so resolve it.
+        destId = dest.startDestination
+    }
+    if (currentDestination?.id != destId) {
+        navigate(directions)
     }
 }

app/src/main/kotlin/com/maltaisn/notes/di/AppComponent.kt
@@ -21,6 +21,9 @@ import com.maltaisn.notes.App
 import com.maltaisn.notes.ui.edit.EditFragment
 import com.maltaisn.notes.ui.home.HomeFragment
 import com.maltaisn.notes.ui.main.MainActivity
+import com.maltaisn.notes.ui.reminder.ReminderDateDialog
+import com.maltaisn.notes.ui.reminder.ReminderDialog
+import com.maltaisn.notes.ui.reminder.ReminderTimeDialog
 import com.maltaisn.notes.ui.search.SearchFragment
 import com.maltaisn.notes.ui.settings.SettingsFragment
 import dagger.BindsInstance
@@ -39,6 +42,9 @@ interface AppComponent {
     fun inject(fragment: SearchFragment)
     fun inject(fragment: EditFragment)
     fun inject(fragment: SettingsFragment)
+    fun inject(dialog: ReminderDialog)
+    fun inject(dialog: ReminderDateDialog)
+    fun inject(dialog: ReminderTimeDialog)
 
     @Component.Factory
     interface Factory {

app/src/main/kotlin/com/maltaisn/notes/model/converter/DateTimeConverter.kt
@@ -54,13 +54,10 @@ object DateTimeConverter : KSerializer<Date> {
     @JvmStatic
     fun toLong(date: Date) = date.time
 
-    // This is only used for testing
-    fun toDate(str: String) = dateFormat.parse(str)!!
-
     override val descriptor = PrimitiveDescriptor("Date", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: Date) =
         encoder.encodeString(dateFormat.format(value))
 
-    override fun deserialize(decoder: Decoder) = toDate(decoder.decodeString())
+    override fun deserialize(decoder: Decoder) = dateFormat.parse(decoder.decodeString())!!
 }

app/src/main/kotlin/com/maltaisn/notes/model/converter/RecurrenceConverter.kt
@@ -33,16 +33,16 @@ object RecurrenceConverter : KSerializer<Recurrence> {
 
     @TypeConverter
     @JvmStatic
-    fun toRecurrence(rrule: String) = rruleFormatter.parse(rrule)
+    fun toRecurrence(rrule: String?) = rrule?.let { rruleFormatter.parse(it) }
 
     @TypeConverter
     @JvmStatic
-    fun toRRule(recurrence: Recurrence) = rruleFormatter.format(recurrence)
+    fun toRRule(recurrence: Recurrence?) = recurrence?.let { rruleFormatter.format(it) }
 
     override val descriptor = PrimitiveDescriptor("Recurrence", PrimitiveKind.STRING)
 
     override fun serialize(encoder: Encoder, value: Recurrence) =
-        encoder.encodeString(toRRule(value))
+        encoder.encodeString(toRRule(value)!!)
 
-    override fun deserialize(decoder: Decoder) = toRecurrence(decoder.decodeString())
+    override fun deserialize(decoder: Decoder) = toRecurrence(decoder.decodeString())!!
 }

commit c95a3562e7d8142d85101d38f25f40733f9bcb86

Fix drawer items checking regression
Introduced in 8452c73.

app/src/main/kotlin/com/maltaisn/notes/ui/home/HomeFragment.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Nicolas Maltais
+ * Copyright 2021 Nicolas Maltais
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -177,6 +177,10 @@ class HomeFragment : NoteFragment(), Toolbar.OnMenuItemClickListener,
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        if (item.isCheckable) {
+            // There are items in two separate group, so checking must be handled manually.
+            navView.setCheckedItem(item)
+        }
         when (item.itemId) {
             R.id.item_location_active -> viewModel.setNoteStatus(NoteStatus.ACTIVE)
             R.id.item_location_archived -> viewModel.setNoteStatus(NoteStatus.ARCHIVED)

app/src/main/res/menu/menu_drawer.xml
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?><!--
-  Copyright 2020 Nicolas Maltais
+  Copyright 2021 Nicolas Maltais
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -19,20 +19,21 @@
 
     <group
         android:id="@+id/group_main_notes"
-        android:checkableBehavior="single"
         >
 
         <item
             android:id="@+id/item_location_active"
-            android:checked="true"
             android:icon="@drawable/ic_text"
             android:title="@string/note_location_active"
+            android:checkable="true"
+            android:checked="true"
             />
 
         <item
             android:id="@+id/item_reminders"
             android:icon="@drawable/ic_alarm"
             android:title="@string/note_reminders"
+            android:checkable="true"
             />
 
     </group>
@@ -44,12 +45,14 @@
             android:id="@+id/item_location_archived"
             android:icon="@drawable/ic_archive"
             android:title="@string/note_location_archived"
+            android:checkable="true"
             />
 
         <item
             android:id="@+id/item_location_deleted"
             android:icon="@drawable/ic_delete"
             android:title="@string/note_location_deleted"
+            android:checkable="true"
             />
 
     </group>