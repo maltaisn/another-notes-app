maltaisn/another-notes-app

commit e374327c259b4659644065c9573c43184984bbd1

Fixed formatting not removed when pasting text

app/src/main/kotlin/com/maltaisn/notes/ui/edit/adapter/EditListViewHolder.kt
@@ -17,7 +17,10 @@
 package com.maltaisn.notes.ui.edit.adapter
 
 import android.text.Editable
+import android.text.InputFilter
+import android.text.Spannable
 import android.view.KeyEvent
+import androidx.core.text.clearSpans
 import androidx.core.view.isInvisible
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
@@ -43,6 +46,7 @@ class EditTitleViewHolder(binding: ItemEditTitleBinding, callback: EditAdapter.C
         titleEdt.setOnClickListener {
             callback.onNoteClickedToEdit()
         }
+        titleEdt.filters = arrayOf(clearSpansInputFilter)
         titleEdt.setHorizontallyScrolling(false)
         titleEdt.maxLines = Integer.MAX_VALUE
     }
@@ -68,6 +72,7 @@ class EditContentViewHolder(binding: ItemEditContentBinding, callback: EditAdapt
     private val contentEdt = binding.contentEdt
 
     init {
+        contentEdt.filters = arrayOf(clearSpansInputFilter)
         contentEdt.addTextChangedListener(BulletTextWatcher())
         contentEdt.setOnClickListener {
             callback.onNoteClickedToEdit()
@@ -103,6 +108,8 @@ class EditItemViewHolder(val binding: ItemEditItemBinding, callback: EditAdapter
         itemCheck.setOnCheckedChangeListener { _, isChecked ->
             item.checked = isChecked
         }
+
+        itemEdt.filters = arrayOf(clearSpansInputFilter)
 
         itemEdt.doOnTextChanged { _, _, _, count ->
             // This is used to detect when user enters line breaks into the input, so the
@@ -185,4 +192,14 @@ private class AndroidEditableText(override val text: Editable) : EditableText {
     override fun replaceAll(text: CharSequence) {
         this.text.replace(0, this.text.length, text)
     }
+}
+
+private val clearSpansInputFilter = InputFilter { source, start, end, _, _, _ ->
+    // Pasted text can often have foreign spans, for example when copying text from the browser.
+    // This filter removes all of them.
+    val filtered = source.subSequence(start, end)
+    if (filtered is Spannable) {
+        filtered.clearSpans()
+    }
+    filtered
 }

commit 1b071bd3a4c3a1c5d0f66408b70b755296cc5d33

Fix broken text input in EditFragment
Introduced in e374327c2

app/src/main/kotlin/com/maltaisn/notes/ui/edit/adapter/EditListViewHolder.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Nicolas Maltais
+ * Copyright 2021 Nicolas Maltais
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,10 +17,10 @@
 package com.maltaisn.notes.ui.edit.adapter
 
 import android.text.Editable
-import android.text.InputFilter
-import android.text.Spannable
+import android.text.TextWatcher
+import android.text.style.CharacterStyle
 import android.view.KeyEvent
-import androidx.core.text.clearSpans
+import androidx.core.text.getSpans
 import androidx.core.view.isInvisible
 import androidx.core.widget.doOnTextChanged
 import androidx.recyclerview.widget.RecyclerView
@@ -47,7 +47,7 @@ class EditTitleViewHolder(binding: ItemEditTitleBinding, callback: EditAdapter.C
         titleEdt.setOnClickListener {
             callback.onNoteClickedToEdit()
         }
-        titleEdt.filters = arrayOf(clearSpansInputFilter)
+        titleEdt.addTextChangedListener(clearSpansTextWatcher)
         titleEdt.setHorizontallyScrolling(false)
         titleEdt.maxLines = Integer.MAX_VALUE
     }
@@ -73,8 +73,8 @@ class EditContentViewHolder(binding: ItemEditContentBinding, callback: EditAdapt
     private val contentEdt = binding.contentEdt
 
     init {
-        contentEdt.filters = arrayOf(clearSpansInputFilter)
         contentEdt.addTextChangedListener(BulletTextWatcher())
+        contentEdt.addTextChangedListener(clearSpansTextWatcher)
         contentEdt.setOnClickListener {
             callback.onNoteClickedToEdit()
         }
@@ -112,7 +112,7 @@ class EditItemViewHolder(val binding: ItemEditItemBinding, callback: EditAdapter
             itemEdt.isActivated = !isChecked // Controls text color selector.
         }
 
-        itemEdt.filters = arrayOf(clearSpansInputFilter)
+        itemEdt.addTextChangedListener(clearSpansTextWatcher)
 
         itemEdt.doOnTextChanged { _, _, _, count ->
             // This is used to detect when user enters line breaks into the input, so the
@@ -196,12 +196,21 @@ private class AndroidEditableText(override val text: Editable) : EditableText {
     }
 }
 
-private val clearSpansInputFilter = InputFilter { source, start, end, _, _, _ ->
-    // Pasted text can often have foreign spans, for example when copying text from the browser.
-    // This filter removes all of them.
-    val filtered = source.subSequence(start, end)
-    if (filtered is Spannable) {
-        filtered.clearSpans()
+private val clearSpansTextWatcher = object : TextWatcher {
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        // nothing
     }
-    filtered
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        // nothing
+    }
+
+    override fun afterTextChanged(s: Editable?) {
+        if (s == null) return
+        // Might not remove all spans but will work for most of them.
+        val spansToRemove = s.getSpans<CharacterStyle>()
+        for (span in spansToRemove) {
+            s.removeSpan(span)
+        }
+    }
 }