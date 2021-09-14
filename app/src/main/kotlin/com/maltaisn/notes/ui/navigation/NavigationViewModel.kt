maltaisn/another-notes-app

commit 848c6031da3aefd196da0a8ae0d98ce41cd8167c

Add hidden attribute for labels
Closes #15

- Fix bug allowing duplicate labels and blank labels
- Fixed bug where the label chip showed in that label's destination after its name was changed.
- Fix label name not restored on process death in label edit dialog
- Fix search & home view models that kept updating list after fragment view was destroyed.
- Reduce dialog vertical insets considerably

app/src/main/kotlin/com/maltaisn/notes/ui/navigation/NavigationViewModel.kt
@@ -154,21 +154,23 @@ class NavigationViewModel @AssistedInject constructor(
     }
 
     private fun restoreSelection(list: MutableList<NavigationItem>) {
-        var selectionFound = false
         for ((i, item) in list.withIndex()) {
             if (item is NavigationDestinationItem && item.id == checkedId) {
                 list[i] = item.copy(checked = true)
-                selectionFound = true
-                break
+                if (item.destination is HomeDestination) {
+                    // Set destination again to make sure observers have the update destination value.
+                    // For example in the case a label, it could become hidden or change name.
+                    _currentHomeDestination.value = item.destination
+                }
+                return
             }
         }
-        if (!selectionFound) {
-            // There's no checked destination. Only way this can happen is a checked label was
-            // removed. Go back to active notes.
-            checkedId = ITEM_ID_ACTIVE
-            list[1] = (list[1] as NavigationDestinationItem).copy(checked = true)
-            _currentHomeDestination.value = HomeDestination.Status(NoteStatus.ACTIVE)
-        }
+
+        // There's no checked destination. Only way this can happen is a checked label was
+        // removed. Go back to active notes.
+        checkedId = ITEM_ID_ACTIVE
+        list[1] = (list[1] as NavigationDestinationItem).copy(checked = true)
+        HomeDestination.Status(NoteStatus.ACTIVE)
     }
 
     private fun MutableList<NavigationItem>.addDivider() {

app/src/androidTest/kotlin/com/maltaisn/notes/model/MigrationTest.kt
@@ -50,7 +50,7 @@ class MigrationTest {
     @Test
     @Throws(IOException::class)
     fun migrate2To3() {
-        // Test that pinned default value is `false`.
+        // Test that pinned default value is `false`, new reminder columns defaults
         helper.createDatabase(DB_NAME, 2).apply {
             execSQL("""
                 INSERT INTO notes (id, type, title, content, metadata, added_date, modified_date, status) 
@@ -71,6 +71,26 @@ class MigrationTest {
         assertNull(cursor.getIntOrNull(4))
         assertNull(cursor.getIntOrNull(5))
         cursor.moveToNext()
+        assertEquals(0, cursor.getInt(0))
+        cursor.close()
+
+        db.close()
+    }
+
+    @Test
+    @Throws(IOException::class)
+    fun migrate3To4() {
+        // Test that default hidden status for label is false
+        helper.createDatabase(DB_NAME, 3).apply {
+            execSQL("""
+                INSERT INTO labels (id, name) VALUES (1, 'label')
+                """.trimIndent())
+            close()
+        }
+        val db = helper.runMigrationsAndValidate(DB_NAME, 4, true, NotesDatabase.MIGRATION_3_4)
+
+        val cursor = db.query("""SELECT hidden FROM labels""")
+        cursor.moveToFirst()
         assertEquals(0, cursor.getInt(0))
         cursor.close()
 

app/src/main/kotlin/com/maltaisn/notes/model/DefaultJsonManager.kt
@@ -166,7 +166,7 @@ class DefaultJsonManager @Inject constructor(
     }
 
     companion object {
-        private const val VERSION = 3
+        private const val VERSION = 4
         private const val FIRST_VERSION = 3
     }
 }

app/src/main/kotlin/com/maltaisn/notes/model/NotesDao.kt
@@ -78,10 +78,15 @@ interface NotesDao {
 
     /**
      * Get all notes with a [status], sorted by last modified date, with pinned notes first.
-     * This is used to display notes for each status.
+     * Exclude notes with a label marked as hidden, except if the note is deleted.
+     * This is used to display notes for each status destination.
      */
     @Transaction
-    @Query("SELECT * FROM notes WHERE status == :status ORDER BY pinned DESC, modified_date DESC, id")
+    @Query("""SELECT * FROM notes WHERE status == :status AND (:status == 2 OR id NOT IN 
+        (SELECT DISTINCT notes.id FROM notes JOIN label_refs ON noteId == notes.id 
+        JOIN labels ON labelId == labels.id WHERE labels.hidden == 1))
+        ORDER BY pinned DESC, modified_date DESC, id
+    """)
     fun getByStatus(status: NoteStatus): Flow<List<NoteWithLabels>>
 
     /**

app/src/main/kotlin/com/maltaisn/notes/model/NotesDatabase.kt
@@ -56,7 +56,7 @@ abstract class NotesDatabase : RoomDatabase() {
 
     @Suppress("MagicNumber")
     companion object {
-        const val VERSION = 3
+        const val VERSION = 4
 
         val MIGRATION_1_2 = object : Migration(1, 2) {
             override fun migrate(database: SupportSQLiteDatabase) {
@@ -105,9 +105,19 @@ abstract class NotesDatabase : RoomDatabase() {
             }
         }
 
+        val MIGRATION_3_4 = object : Migration(3, 4) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.apply {
+                    // Add hidden attribute for labels
+                    execSQL("ALTER TABLE labels ADD COLUMN hidden INTEGER NOT NULL DEFAULT 0")
+                }
+            }
+        }
+
         val ALL_MIGRATIONS = arrayOf(
             MIGRATION_1_2,
             MIGRATION_2_3,
+            MIGRATION_3_4,
         )
     }
 }

app/src/main/kotlin/com/maltaisn/notes/model/entity/Label.kt
@@ -44,11 +44,14 @@ data class Label(
      */
     @ColumnInfo(name = "name", index = true)
     val name: String,
-) : Parcelable {
 
-    init {
-        require(name.isNotBlank()) { "Blank label name" }
-    }
+    /**
+     * Whether the notes with this label will be hidden from active/archived destinations.
+     * These notes will only be visible in the label destinations.
+     */
+    @ColumnInfo(name = "hidden")
+    val hidden: Boolean = false,
+) : Parcelable {
 
     companion object {
         const val NO_ID = 0L

app/src/main/kotlin/com/maltaisn/notes/ui/home/HomeFragment.kt
@@ -145,7 +145,6 @@ class HomeFragment : NoteFragment(), Toolbar.OnMenuItemClickListener {
             }
             is HomeDestination.Labels -> destination.label.name
             is HomeDestination.Reminders -> getString(R.string.note_reminders)
-            else -> error("Unknown destination")
         }
     }
 

app/src/main/kotlin/com/maltaisn/notes/ui/home/HomeViewModel.kt
@@ -43,7 +43,6 @@ import com.maltaisn.notes.ui.note.adapter.NoteListItem
 import com.maltaisn.notes.ui.send
 import com.squareup.inject.assisted.Assisted
 import com.squareup.inject.assisted.AssistedInject
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 import java.util.Calendar
@@ -57,8 +56,6 @@ class HomeViewModel @AssistedInject constructor(
     private val buildTypeBehavior: BuildTypeBehavior,
 ) : NoteViewModel(savedStateHandle, notesRepository, labelsRepository, prefs, reminderAlarmManager),
     NoteAdapter.Callback {
-
-    private var noteListJob: Job? = null
 
     var currentDestination: HomeDestination = HomeDestination.Status(NoteStatus.ACTIVE)
         private set
@@ -293,7 +290,8 @@ class HomeViewModel @AssistedInject constructor(
 
             val checked = isNoteSelected(note)
             // Omit the filtered label from the note since all notes have it.
-            this += NoteItem(note.id, note, noteWithLabels.labels - label, checked)
+            val labels = noteWithLabels.labels - label
+            this += NoteItem(note.id, note, labels, checked)
         }
     }
 

commit 7d6ac0b74e34e53ed2012f2aa6ed51316a9a94a4

Fix destination not changed after label deletion
Regression introduced in 848c6031

app/src/main/kotlin/com/maltaisn/notes/ui/navigation/NavigationViewModel.kt
@@ -170,7 +170,7 @@ class NavigationViewModel @AssistedInject constructor(
         // removed. Go back to active notes.
         checkedId = ITEM_ID_ACTIVE
         list[1] = (list[1] as NavigationDestinationItem).copy(checked = true)
-        HomeDestination.Status(NoteStatus.ACTIVE)
+        _currentHomeDestination.value = HomeDestination.Status(NoteStatus.ACTIVE)
     }
 
     private fun MutableList<NavigationItem>.addDivider() {