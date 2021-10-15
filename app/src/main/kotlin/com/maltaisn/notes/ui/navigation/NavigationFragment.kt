0be6b2bc5accc5b4341141be43987a025f4fc20a
maltaisn <nicolas.g.maltais@gmail.com>
2021-10-15 08:41:34 -0400

    Gradle update, Android Studio update
    Version bump

--- app/src/main/kotlin/com/maltaisn/notes/ui/navigation/NavigationFragment.kt
+++ app/src/main/kotlin/com/maltaisn/notes/ui/navigation/NavigationFragment.kt
@@ -105,6 +105,9 @@ class NavigationFragment : Fragment() {
         }
 
         requireView().post {
+            // Somewhat ugly: post is used to delay sharedViewModel creation since this fragment
+            // is created alongside the NavigationFragment and thus the navigation controller
+            // doesn't (fully?) exist yet.
             sharedViewModel.labelAddEvent.observeEvent(viewLifecycleOwner) { label ->
                 // If user is on home fragment, select the newly created label.
                 if (findNavController().previousBackStackEntry?.destination?.id == R.id.fragment_home) {
