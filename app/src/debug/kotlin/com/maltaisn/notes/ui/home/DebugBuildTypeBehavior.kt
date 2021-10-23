849f65844621b84a0e13afc7228b471f10808a30
maltaisn <nicolas.g.maltais@gmail.com>
2021-10-23 13:40:10 -0400

    Debug action allow adding notes in label destination

--- app/src/debug/kotlin/com/maltaisn/notes/ui/home/DebugBuildTypeBehavior.kt
+++ app/src/debug/kotlin/com/maltaisn/notes/ui/home/DebugBuildTypeBehavior.kt
@@ -17,13 +17,16 @@
 package com.maltaisn.notes.ui.home
 
 import com.maltaisn.notes.DebugUtils
+import com.maltaisn.notes.model.LabelsRepository
 import com.maltaisn.notes.model.NotesRepository
+import com.maltaisn.notes.model.entity.LabelRef
+import com.maltaisn.notes.model.entity.NoteStatus
 import com.maltaisn.notes.ui.navigation.HomeDestination
 import javax.inject.Inject
 
 class DebugBuildTypeBehavior @Inject constructor(
     private val notesRepository: NotesRepository,
-//    private val labelsRepository: LabelsRepository
+    private val labelsRepository: LabelsRepository,
 ) : BuildTypeBehavior {
 
     override suspend fun doExtraAction(viewModel: HomeViewModel) {
@@ -34,6 +37,7 @@ class DebugBuildTypeBehavior @Inject constructor(
                 notesRepository.insertNote(DebugUtils.getRandomNote(destination.status))
             }
 
+            // For performance testing
 //            val labels = (1L..100L).map { Label(it, UUID.randomUUID().toString().substring(0, 9)) }
 //            for (label in labels) {
 //                labelsRepository.insertLabel(label)
@@ -45,6 +49,11 @@ class DebugBuildTypeBehavior @Inject constructor(
 //                    labelsRepository.insertLabelRefs(noteLabels.map { LabelRef(id, it.id) })
 //                }
 //            }
+        } else if (destination is HomeDestination.Labels) {
+            repeat(3) {
+                val id = notesRepository.insertNote(DebugUtils.getRandomNote(NoteStatus.ACTIVE))
+                labelsRepository.insertLabelRefs(listOf(LabelRef(id, destination.label.id)))
+            }
         }
     }
 }
